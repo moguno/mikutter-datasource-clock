b0e02ef9148b47e8148aea30ba3ad6e24ed76c7e
moguno <moguno@0kn.sakura.ne.jp>
2015-03-28 15:10:23 +0900

    マルチアカウント時に二重に時刻が出力されるのを解決

--- mikutter-datasource-clock.rb
+++ mikutter-datasource-clock.rb
@@ -8,9 +8,11 @@ Plugin.create(:mikutter_datasource_clock) {
   }
 
   on_period { |service|
-    time = "現在の時刻は #{DateTime.now.strftime("%Y/%m/%d (%a) %H:%M")} だよ"
-    message = Message.new(:message => time, :system => true)
+    if service == Service.primary
+      time = "現在の時刻は #{DateTime.now.strftime("%Y/%m/%d (%a) %H:%M")} だよ"
+      message = Message.new(:message => time, :system => true)
 
-    Plugin.call(:extract_receive_message, :mikutter_datasource_clock, Messages.new([message]))
+      Plugin.call(:extract_receive_message, :mikutter_datasource_clock, Messages.new([message]))
+    end
   }
 }
